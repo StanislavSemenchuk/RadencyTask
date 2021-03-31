b63eb44c07a60e8d03b0dc5f8452ed7b743ed0f7
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2021-03-31 14:24:20 +0300

    first implementation

--- bin/radency_test.dart
+++ bin/radency_test.dart
@@ -2,5 +2,5 @@ import 'package:radency_test/distanceCalculator.dart' as calculator;
 
 void main(List<String> arguments) {
   var calc = calculator.DistanceCalculator();
-  print('Hello world: ${calc.chooseBestDistance(0, 0, [1, 2, 3])}!');
+  print('Hello world: ${calc.chooseBestDistance(163, 3, [50])}!');
 }
--- lib/distanceCalculator.dart
+++ lib/distanceCalculator.dart
@@ -1,6 +1,22 @@
+import 'package:trotter/trotter.dart';
+
 class DistanceCalculator {
   int chooseBestDistance(int t, int k, List<int> ls) {
-    // твій код
-    return -1;
+    //твій код
+    if (k < 1 || t < 0 || ls.isEmpty || ls.length < t) {
+      return -1;
+    }
+
+    var combList = Combinations(k, ls);
+    var dist = 0;
+
+    for (List item in combList.iterable) {
+      var temp = item.map((e) => e).reduce((v, e) => v + e);
+      if (temp <= t && temp > dist) {
+        dist = temp;
+      }
+    }
+
+    return dist;
   }
 }
