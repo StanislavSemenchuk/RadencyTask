b677dac583025ad5c37909e04f9fb041da90c40b
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2021-03-31 14:29:49 +0300

    v0.0.1

--- bin/radency_test.dart
+++ bin/radency_test.dart
@@ -2,5 +2,6 @@ import 'package:radency_test/distanceCalculator.dart' as calculator;
 
 void main(List<String> arguments) {
   var calc = calculator.DistanceCalculator();
-  print('Hello world: ${calc.chooseBestDistance(163, 3, [50])}!');
+  print(
+      'Hello world: ${calc.chooseBestDistance(174, 3, [51, 56, 58, 59, 61])}!');
 }
--- test/radency_test_test.dart
+++ test/radency_test_test.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
+import 'package:radency_test/distanceCalculator.dart' as calculator;
 
 void main() {
-  test('calculate', () {});
+  test('calculateWithAllowableNumbers', () {});
 }
